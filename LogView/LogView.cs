5254b042a99c21666009491b5fcdc366c8ef4bf5
ajkfds <ajkfds2018@gmail.com>
2018-12-05 08:04:25 +0900

    refactor logview

--- LogView/LogView.cs
+++ LogView/LogView.cs
@@ -17,48 +17,83 @@ namespace ajkControls
             InitializeComponent();
         }
 
-        private Log log = new Log();
+        private List<string> logs = new List<string>();
+        private List<System.Drawing.Color> logColors = new List<Color>();
+        private List<int> logHeights = new List<int>();
+
+        private int maxLogs = 200;
+        public int MaxLogs {
+            get
+            {
+                return maxLogs;
+            }
+            set
+            {
+                maxLogs = value;
+            }
+        }
+
+        private void appendLog(string log,Color color)
+        {
+            logs.Add(log);
+            logColors.Add(color);
+            logHeights.Add(-1);
+            while (logs.Count > MaxLogs)
+            {
+                logs.RemoveAt(0);
+                logColors.RemoveAt(0);
+                logHeights.RemoveAt(0);
+            }
+        }
+
 
         private void dbDrawBox_Resize(object sender, EventArgs e)
         {
             dbDrawBox.Invalidate();
+            for (int i = 0; i < logHeights.Count; i++)
+            {
+                logHeights[i] = -1;
+            }
         }
 
-        private List<string> logsFromAnotherThread = new List<string>();
-        public void AppendLogLineFromThread(string logLine)
+        private List<string> logStock = new List<string>();
+        private List<Color> logColorStock = new List<Color>();
+        public void AppendLogLine(string logLine)
         {
-            lock (logsFromAnotherThread)
+            lock (logStock)
             {
-                logsFromAnotherThread.Add(logLine);
+                logStock.Add(logLine);
+                logColorStock.Add(ForeColor);
+            }
+        }
+        public void AppendLogLine(string logLine,Color color)
+        {
+            lock (logStock)
+            {
+                logStock.Add(logLine);
+                logColorStock.Add(color);
             }
         }
 
         private void timer_Tick(object sender, EventArgs e)
         {
-            lock (logsFromAnotherThread)
+            lock (logStock)
             {
-                if(logsFromAnotherThread.Count == 0)
+                if(logStock.Count == 0)
                 {
                     return;
                 }
 
-                foreach(string line in logsFromAnotherThread)
+                for(int i = 0; i < logStock.Count; i++)
                 {
-                    log.AppendLogLine(line);
+                    appendLog(logStock[i], logColorStock[i]);
                 }
-                logsFromAnotherThread.Clear();
+
+                logStock.Clear();
+                logColorStock.Clear();
             }
             dbDrawBox.Invalidate();
         }
-
-        public void AppendLogLine(string logLine)
-        {
-            log.AppendLogLine(logLine);
-            dbDrawBox.Invalidate();
-        }
-
-
-        int startLine = 0;
 
         private void dbDrawBox_DoubleBufferedPaint(PaintEventArgs e)
         {
@@ -66,22 +101,39 @@ namespace ajkControls
 
             TextFormatFlags flags = TextFormatFlags.Top | TextFormatFlags.WordBreak;
 
-            int i = 0;
-            int y = 0;
-            while (i<log.LineCount && y<dbDrawBox.Height)
+            for(int i = 0; i < logs.Count; i++)
             {
-                string line = log.LineText(i+startLine);
+                if (logHeights[i] != -1) continue;
+                string line = logs[i];
                 if (line == "") line = " ";
-                Size size = System.Windows.Forms.TextRenderer.MeasureText(e.Graphics,line, Font,new Size(dbDrawBox.Width,dbDrawBox.Height),flags);
-                Rectangle rect = new Rectangle(0, y, dbDrawBox.Width, dbDrawBox.Height);
-                System.Windows.Forms.TextRenderer.DrawText(e.Graphics, line, Font, rect, Color.Gray,flags);
-                y = y + size.Height;
-                i++;
+                Size size = System.Windows.Forms.TextRenderer.MeasureText(e.Graphics, line, Font, new Size(dbDrawBox.Width, dbDrawBox.Height), flags);
+                logHeights[i] = size.Height;
             }
-            if (y >= dbDrawBox.Height)
+
+            if (logHeights.Count == 0) return;
+            int startline = logHeights.Count - 1;
+
+            int y = 0;
+            while (startline > 0)
             {
-                startLine++;
-                Invalidate();
+                y = y + logHeights[startline];
+                if (y >= dbDrawBox.Height)
+                {
+                    startline++;
+                    break;
+                }
+                startline--;
+            }
+
+            y = 0;
+            int j = startline;
+            while (j < logs.Count && y<dbDrawBox.Height)
+            {
+                string line = logs[j];
+                Rectangle rect = new Rectangle(0, y, dbDrawBox.Width, dbDrawBox.Height);
+                System.Windows.Forms.TextRenderer.DrawText(e.Graphics, line, Font, rect, logColors[j],flags);
+                y = y + logHeights[j];
+                j++;
             }
         }
 
