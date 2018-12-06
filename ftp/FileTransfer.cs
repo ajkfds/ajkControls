fffa90853cc0c7370e3353d9e943129a1223034d
ajkfds <ajkfds2018@gmail.com>
2018-12-07 06:13:26 +0900

    add filetransfer class

---
+++ ftp/FileTransfer.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ajkControls
+{
+    public abstract class FileTransfer: IDisposable
+    {
+        public class MessageReceivedEventArgs : EventArgs
+        {
+            public MessageReceivedEventArgs(string message)
+            {
+                this.Message = message;
+            }
+
+            public string Message;
+        }
+
+        public delegate void MessageReceivedEventHandler(object sender, MessageReceivedEventArgs e);
+        public event MessageReceivedEventHandler MessageReceived;
+
+        protected virtual void OnMessageReceived(MessageReceivedEventArgs e)
+        {
+            if (MessageReceived != null) MessageReceived(this, e);
+        }
+
+        public void Dispose() { }
+
+        public bool KeepAlive { get; set; }
+
+        public virtual bool DownloadFile(string localPath, string serverPath)
+        {
+            return false;
+        }
+
+        public virtual bool UploadFile(string serverPath, string localPath)
+        {
+            return false;
+        }
+
+        public virtual bool GetFileList(string serverPath, out List<string> fileList)
+        {
+            fileList = new List<string>();
+            return false;
+        }
+
+        public virtual bool GetLastUpdate(string serverPath, out DateTime dateTime)
+        {
+            dateTime = DateTime.MinValue;
+            return false;
+        }
+
+        public virtual bool MakeDirectory(string serverPath)
+        {
+            return false;
+        }
+    }
+}
---
+++ ftp/FtpClient.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ajkControls
+{
+    public class FtpClient : FileTransfer
+    {
+        public FtpClient(string serverUri)
+        {
+            this.serverUri = serverUri;
+        }
+        private string serverUri;
+
+        public override bool DownloadFile(string localPath,string serverPath)
+        {
+            System.Net.FtpWebRequest ftpRequest = (System.Net.FtpWebRequest)
+            System.Net.WebRequest.Create( new Uri(serverUri+serverPath));
+            ftpRequest.Credentials = new System.Net.NetworkCredential("username", "password");
+            ftpRequest.Method = System.Net.WebRequestMethods.Ftp.DownloadFile;
+            ftpRequest.KeepAlive = KeepAlive;
+            ftpRequest.UseBinary = false;
+            ftpRequest.UsePassive = false;
+
+            using (System.Net.FtpWebResponse ftpResponse = (System.Net.FtpWebResponse)ftpRequest.GetResponse())
+            {
+                using (System.IO.Stream responseStream = ftpResponse.GetResponseStream())
+                {
+                    using (System.IO.FileStream fs = new System.IO.FileStream(localPath, System.IO.FileMode.Create, System.IO.FileAccess.Write))
+                    {
+                        byte[] buffer = new byte[1024];
+                        while (true)
+                        {
+                            int readSize = responseStream.Read(buffer, 0, buffer.Length);
+                            if (readSize == 0) break;
+                            fs.Write(buffer, 0, readSize);
+                        }
+                    }
+
+                }
+                if(ftpResponse.StatusCode != System.Net.FtpStatusCode.CommandOK && ftpResponse.StatusCode != System.Net.FtpStatusCode.FileActionOK)
+                {
+                    OnMessageReceived(new MessageReceivedEventArgs(ftpResponse.StatusDescription));
+                    return false;
+                }
+            }
+
+            OnMessageReceived(new MessageReceivedEventArgs(serverUri+serverPath+" download complete"));
+            return true;
+        }
+
+
+    }
+}
